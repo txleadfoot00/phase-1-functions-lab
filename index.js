cb876fa4c4337b5cc4e9d2870e380b7062fb2e9a
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-14 22:45:49 -0700

    commit tree cd68b7b941b47fe211a42e7ea8b52ea93b2de01b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue);
+}
+function distanceFromHqInFeet(someValue) {
+    return 264*distanceFromHqInBlocks(someValue);
+}
+function distanceTravelledInFeet(start, destination) {
+    return 264*Math.abs(start - destination);
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination)<=400) {
+        return 0;
+    }
+    else if ((distanceTravelledInFeet(start, destination)<=2000)) {
+        return .02*((distanceTravelledInFeet(start, destination)-400));
+    }
+    else if ((distanceTravelledInFeet(start, destination)<=2500)) {
+        return 25;
+    }
+    else {
+        return "cannot travel that far";
+    }}
